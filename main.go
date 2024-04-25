7e0c9e02194f7ed9de865ad1e38cf2ccf498c6bb
gryzlegrizz <gryzlegrizz@gmail.com>
2024-04-25 23:29:39 +0700

    p week 5

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"log"
 
-	"iteung/config"
+	"github.com/gryzlegrizz/ws-gilang2024/config"
 
 	"github.com/aiteung/musik"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
 
-	"iteung/url"
+	"github.com/gryzlegrizz/ws-gilang2024/url"
 
 	"github.com/gofiber/fiber/v2"
 )
--- url/url.go
+++ url/url.go
@@ -1,7 +1,7 @@
 package url
 
 import (
-	"iteung/controller"
+	"github.com/gryzlegrizz/ws-gilang2024/controller"
 
 	"github.com/gofiber/fiber/v2"
 )
